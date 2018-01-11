a0e7a5a09b7798a70134c325d1fcb0c0e97ca5f3
Gaelle <gaelle@shopus.fr>
2018-01-11 17:26:51 +0100

    first commit

---
+++ src/pages/setting/setting.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage/dist/storage';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the SettingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-setting',
+  templateUrl: 'setting.html',
+})
+export class SettingPage {
+  
+  city:string;
+  state:string;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private storage:Storage) {
+      this.storage.get('location').then( (val) => {
+        if (val != null ) {
+          let location = JSON.parse(val);
+          this.city = location.city;
+          this.state = location.state;
+        } else {
+          this.city = 'Paris';
+          this.state = 'FR';
+        }
+      });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SettingPage');
+  }
+
+  saveForm() {
+    let location = {
+      city: this.city,
+      state: this.state
+    }
+    //console.log(location);
+    this.storage.set('location', JSON.stringify(location));
+    this.navCtrl.push(HomePage);
+  }
+
+}
